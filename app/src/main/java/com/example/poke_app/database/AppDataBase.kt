b910794c6bbe4a031556ab0443109e72e9f9c71e
gabitheiss <t_gaaby@hotmail.com>
2021-08-23 13:40:05 -0300

    DAO

--- app/src/main/java/com/example/poke_app/database/AppDataBase.kt
+++ app/src/main/java/com/example/poke_app/database/AppDataBase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.poke_app.database.dao.PokemonDAO
 import com.example.poke_app.model.Pokemon
 
 @Database(entities = [Pokemon::class], version = 1)
 abstract class AppDataBase : RoomDatabase() {
+
+    abstract fun pokemonDAO() : PokemonDAO
 
     companion object {
         @Volatile
--- app/src/main/java/com/example/poke_app/database/dao/PokemonDAO.kt
+++ app/src/main/java/com/example/poke_app/database/dao/PokemonDAO.kt
@@ -9,9 +9,15 @@ import com.example.poke_app.model.Pokemon
 @Dao
 interface PokemonDAO {
 
+    //select para retornar todos
     @Query("SELECT * FROM Pokemon ORDER BY poke_name" )
     fun all() : List<Pokemon>
 
+    //select para retornar somente um
+    @Query("SELECT * FROM Pokemon WHERE poke_name = :pokeId" )
+    fun byId(pokeId : String) : Pokemon
+
+    //insert para inserir no banco
     @Insert
     fun insert(pokemon: Pokemon)
 
